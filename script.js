df9f170ec8c34df33f1aa90ae76d5ce93710f8bf
meciatsire <108869800+meciatsire@users.noreply.github.com>
2022-07-08 12:32:31 +0200

    my first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(fuction(){
+    /*This is exucuted after the DOM has been completely loaded */
+
+    $('nav a,footer a.up').click(function(e){
+
+        //If a link has been clicked, scroll the page to the link's
+        target:
+           $.scrollTO(this harsh || 0, 1500);
+           e.preventDefault();
+      });
+
+  });
